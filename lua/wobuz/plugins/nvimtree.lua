c644bd910b294ea49e7046386ec46a33b7e15d94
renlanglang <renlanglang>
2023-05-19 11:08:48 +0800

    fix:bufferline 修复

--- lua/wobuz/plugins/nvimtree.lua
+++ lua/wobuz/plugins/nvimtree.lua
@@ -4,7 +4,6 @@ return {
     dependencies = {"nvim-tree/nvim-web-devicons"},
     config = function()
         require("nvim-tree").setup {
-
             update_focused_file = {enable = true, update_cwd = true},
             renderer = {
                 root_folder_modifier = ":t",
