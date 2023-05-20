7744e1973f5a8b805da66371760fc937f5984dce
wuhuarou <wuhuarou2019@qq.com>
2023-05-20 17:40:40 +0800

    add:alpha

---
+++ lua/wobuz/plugins/alpha.lua
@@ -0,0 +1,12 @@
+return {
+    'goolord/alpha-nvim',
+    dependencies = {'nvim-tree/nvim-web-devicons'},
+    config = function()
+        local status_ok, alpha = pcall(require, "alpha")
+        if not status_ok then return end
+
+        local theme_ok, alpha_theme = pcall(require, "alpha.themes.startify")
+        if not theme_ok then return end
+        alpha.setup(alpha_theme.config)
+    end
+}
