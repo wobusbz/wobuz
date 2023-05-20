8ecc4c087537f1313e8a4f859acd6b9eb5557ec4
wuhuarou <wuhuarou2019@qq.com>
2023-05-20 10:39:57 +0800

    add:golang gopls 代码提升，保存时自动格式化以及import包

--- lua/wobuz/plugins/lspconfig.lua
+++ lua/wobuz/plugins/lspconfig.lua
@@ -1,19 +1,28 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
     config = function()
+        local function lsp_highlight_document(client)
+            -- Set autocommands conditional on server_capabilities
+            local status_ok, illuminate = pcall(require, "illuminate")
+            if not status_ok then return end
+            illuminate.on_attach(client)
+            -- end
+        end
+
         -- Add additional capabilities supported by nvim-cmp
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+       
+        local cmp = require('cmp')
         local lspconfig = require('lspconfig')
-
-        -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-        local servers = {'clangd', 'go'}
-        for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup {
-                -- on_attach = my_custom_on_attach,
-                capabilities = capabilities
-            }
-        end
+        lspconfig.gopls.setup({
+            on_attach = function(client, buff)
+                -- require('lsp_signature').on_attach() -- 如果你想使用函数签名插件，可以添加这行代码
+                lsp_highlight_document(client)
+            end,
+            settings = {
+                gopls = {analyses = {unusedparams = true}, staticcheck = true}
+            },
+            capabilities = capabilities
+        })
     end
 }
--- lua/wobuz/plugins/nvimtreesitter.lua
+++ lua/wobuz/plugins/nvimtreesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    -- build = ":TSUpdate",
     event = "BufReadPost",
     config = function()
         require'nvim-treesitter.configs'.setup {
