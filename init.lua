3e2cfe6fea61ce87f2136db542ca9a9f668710b6
wuhuarou <wuhuarou2019@qq.com>
2023-05-20 21:02:24 +0800

    fix:timeoutlen时间过短导致按键不响应,只有在插入的时候动态去改变这个时间

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("wobuz.config.options")
 require("wobuz.config.keymaps")
+require("wobuz.config.autocmds")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- lua/wobuz/config/autocmds.lua
+++ lua/wobuz/config/autocmds.lua
@@ -1,1 +1,12 @@
 -- vim.cmd('autocmd BufEnter * lua require("completion").on_attach()')
+
+
+vim.cmd([[
+    augroup TimeoutlenSettings
+    autocmd!
+    autocmd InsertEnter * set timeoutlen=30
+    autocmd InsertEnter * set updatetime=30
+    autocmd InsertLeave * set timeoutlen=300
+    autocmd InsertLeave * set updatetime=300
+  augroup END
+]])
