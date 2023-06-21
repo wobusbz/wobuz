254628a910119f54e65f689fed345cc9fcd3e677
wuhuarou <wuhuarou2019@qq.com>
2023-06-21 19:41:24 +0800

    add:clangd lsp

--- lua/wobuz/plugins/lsp.lua
+++ lua/wobuz/plugins/lsp.lua
@@ -12,6 +12,8 @@ return {
         dependencies = {"neovim/nvim-lspconfig"},
         config = function()
             local handler = require("wobuz.plugins.setting.handlers")
+
+            local lspconfig = require("lspconfig")
             require("mason-lspconfig").setup({
                 handlers = {
                     function(server_name) -- default handler (optional)
@@ -32,13 +34,21 @@ return {
                             opts = vim.tbl_deep_extend("force",
                                                        require "wobuz.plugins.setting.clangd",
                                                        opts)
+                            opts.root_pattern =
+                                lspconfig.util.root_pattern('.clangd',
+                                                            '.clang-tidy',
+                                                            '.clang-format',
+                                                            'compile_commands.json',
+                                                            'compile_flags.txt',
+                                                            'configure.ac',
+                                                            '.git')
                         end
                         if server_name == "lua_ls" then
                             opts = vim.tbl_deep_extend("force",
                                                        require "wobuz.plugins.setting.lua_ls",
                                                        opts)
                         end
-                        require("lspconfig")[server_name].setup(opts)
+                        lspconfig[server_name].setup(opts)
                     end
                 }
             })
--- lua/wobuz/plugins/null-ls.lua
+++ lua/wobuz/plugins/null-ls.lua
@@ -25,7 +25,12 @@ return {
                         group = augroup,
                         buffer = bufnr,
                         callback = function()
-                            vim.lsp.buf.format({bufnr = bufnr})
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                filter = function(client)
+                                    return client.name == "null-ls"
+                                end
+                            })
                         end
                     })
                 end
